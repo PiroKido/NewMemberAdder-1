d194eee107f44bb6cb35fee3c15600e9e834dedb
Vivek <123234657+VivekCoderOp@users.noreply.github.com>
2023-04-26 03:34:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ import csv
 #add_user= query_msg= users_info=0
 if not os.path.exists('./sessions'):
     os.mkdir('./sessions')
-if not os.path.exists(f"Users/1955509952/phone.csv"):
+if not os.path.exists(f"Users/5719367404/phone.csv"):
    os.mkdir('./Users')
-   os.mkdir(f'./Users/1955509952')
-   open(f"Users/1955509952/phone.csv","w")
+   os.mkdir(f'./Users/5719367404')
+   open(f"Users/5719367404/phone.csv","w")
 if not os.path.exists('data.csv'):
     open("data.csv","w")
-UPDATES_CHANNEL = "rjbr0"
-OWNER= [5921492080,5921492080]
-PREMIUM=[6285755686,6285755686]
+UPDATES_CHANNEL = "NOOBXCREATOR"
+OWNER= [5719367404,5656382791]
+PREMIUM=[5656382791,5719367404]
 app = pyrogram.Client("app", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 with open("data.csv", encoding='UTF-8') as f:
