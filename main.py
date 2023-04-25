728a1d2bb908194ac9925744ae053b4582960d91
Vivek <123234657+VivekCoderOp@users.noreply.github.com>
2023-04-26 03:49:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ import csv
 #add_user= query_msg= users_info=0
 if not os.path.exists('./sessions'):
     os.mkdir('./sessions')
-if not os.path.exists(f"Users/5719367404/phone.csv"):
+if not os.path.exists(f"Users/2056781888/phone.csv"):
    os.mkdir('./Users')
-   os.mkdir(f'./Users/5719367404')
-   open(f"Users/5719367404/phone.csv","w")
+   os.mkdir(f'./Users/2056781888')
+   open(f"Users/2056781888/phone.csv","w")
 if not os.path.exists('data.csv'):
     open("data.csv","w")
 UPDATES_CHANNEL = "NOOBXCREATOR"
